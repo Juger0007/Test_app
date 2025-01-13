4aacddb9d1eb62680d6332bcfdfe40a1396c1964
Juger0007 <DavidAntoine.RuizGutierrez@Honeywell.com>
2025-01-13 01:02:41 -0800

    new pagepiling

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,7 +22,10 @@ namespace TestApp.Controllers
         {
             return View();
         }
-
+        public IActionResult Test()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
